031679eefaf5742f4b2adf1d578229bcf1747929
Prof. Fernando <pf1963@fiap.com.br>
2024-06-06 12:07:46 -0300

    Atualizando atraves de uma nova tela

--- src/screens/ScreenUpdate.tsx
+++ src/screens/ScreenUpdate.tsx
@@ -5,30 +5,30 @@ import {updateDoc,db,doc} from '../services/firebaseConfig'
 const ScreenUpdate = (props)=>{
 
     //const title = props.route.params.item
-    console.log(props.route.params.item)
-    const data = props.route.params.title
+    console.log(props.route.params.title)
+    console.log(props.route.params.id)
 
-    const[newTitle,setTitle]=useState(props.route.params.item)
+    const[newTitle,setTitle]=useState(props.route.params.title)
 
-    const updateIsTitle = async(title) => {
+    const updateIsTitle = async(newTitle) => {
         const itemRef = doc(db, "produtos", props.route.params.id);
         
         await updateDoc(itemRef, {
-            title: title
+            title: newTitle
         });
     }
 
 
     return(
         <View>
-            <Text>Nome Produto: {data}</Text>
+            <Text>Nome Produto: {props.route.params.title}</Text>
             <Text>Deseja Atualizar o nome do produtos?</Text>
             <TextInput
                 placeholder="Digite para atualizar o nome do produto"
                 value={newTitle}
                 onChangeText={(value)=>setTitle(value)}
             />
-            <Button title="Atualizar" onPress={updateIsTitle}/>
+            <Button title="Atualizar" onPress={()=>updateIsTitle(newTitle)}/>
         </View>
     )
 }
